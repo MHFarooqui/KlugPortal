08f440b69270858ac88d58bf004744e4f72a5ca1
MHFarooqui <farooquihanif02@gmail.com>
2024-12-03 18:49:35 +0530

    corrected login

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -27,7 +27,7 @@ const Login = () => {
 
   const onLogin = async () => {
     try {
-      const response = await fetch("http://localhost:10000/api/admin/login", {
+      const response = await fetch("https://eklearnapi.onrender.com/api/admin/login", {
         method: "POST",
         headers: {
           'Content-Type': 'application/json'
